8f333ed9d741bf86440a5d297e10e4063c32f5cb
Tabithaokoth <kobbitho@gmail.com>
2024-03-21 13:07:47 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 =31;
+let num2 =2;
+
+let multiply = num1 * num2;
+console.log(multiply); //Output : 62
+
+let random = Math.floor(Math.random() * 5)  +1;
+ 
+let num3 = 20;
+let num4 = 5;
+let mod = (num3/num4);
+
+let max = (2,6,14,15,20)
+max = Math.max(max)
+ 
